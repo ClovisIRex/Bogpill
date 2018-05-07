405d3272556171c9ef6c8b1840e2f1816daa987d
Tal Livny <tal@totango.com>
2018-05-07 10:16:48 +0300

    tweaks for args

--- install.sh
+++ install.sh
@@ -4,8 +4,14 @@ chmod +x ./bogpill.sh
 mkdir -p ~/tmp/bogpill
 cp ./bogpill.sh ~/tmp/bogpill/
 
-# Change to your .rc file according to your shell. I'm too lazy to make it efficient andautocheck it. Zsh is superior anyways
 
-RC_FILE=~/.zshrc
+
+if [ $# -eq 0 ]
+  then
+    RC_FILE=~/.bashrc
+  else
+      RC_FILE=$1
+fi
+
 
 echo "alias bog="~/tmp/bogpill/bogpill.sh"" >> $RC_FILE
